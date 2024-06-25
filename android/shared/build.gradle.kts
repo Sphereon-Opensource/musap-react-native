b364f8e0d897ffe005157b1fca8da672f50161e7
sanderPostma <spostma@sphereon.com>
2024-06-25 14:53:06 +0200

    Cleanup

--- android/shared/build.gradle.kts
+++ android/shared/build.gradle.kts
@@ -11,38 +11,6 @@ kotlin {
             }
         }
     }
-    
-//    listOf(
-//        iosX64(),
-//        iosArm64(),
-//        iosSimulatorArm64()
-//    ).forEach {
-//        it.binaries.framework {
-//            baseName = "shared"
-//        }
-//    }
-
-//    js {
-//        nodejs {
-//
-//        }
-//        binaries.executable()
-//    }
-
-    sourceSets {
-
-        commonMain.dependencies {
-            implementation("com.facebook.react:react-android")
-            implementation("com.squareup.okhttp3:okhttp:4.10.0")
-            implementation("com.google.code.gson:gson:2.8.8")
-            implementation ("org.slf4j:slf4j-api:2.0.7")
-            implementation("org.bouncycastle:bcpkix-jdk15to18:1.71")
-            implementation (files("libs/nimbus-jose-jwt-9.21.jar"))
-            implementation(files("libs/app-release.aar"))
-            implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-            implementation("com.google.firebase:firebase-messaging")
-        }
-    }
 }
 
 android {
